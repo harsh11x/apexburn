077b5a83516e00a95a55b8a82b63bc4c9215b4a9
Harsh <63919951+harsh11x@users.noreply.github.com>
2024-10-20 07:58:38 +0100

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\harsh\\dev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\harsh\\Documents\\apexburn" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.0.1+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\harsh\\dev\\flutter"
+  "PROJECT_DIR=C:\\Users\\harsh\\Documents\\apexburn"
+  "FLUTTER_ROOT=C:\\Users\\harsh\\dev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\harsh\\Documents\\apexburn\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\harsh\\Documents\\apexburn"
+  "FLUTTER_TARGET=lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=true"
+  "PACKAGE_CONFIG=C:\\Users\\harsh\\Documents\\apexburn\\.dart_tool\\package_config.json"
+)
